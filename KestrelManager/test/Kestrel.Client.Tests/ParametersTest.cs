3c3c8014c9cef1a471c652c6479a98f43e4609cb
Sergey Voyteshonok <sergeyvoyteshonok@ya.ru>
2016-12-06 15:43:46 +0300

    added tests

---
+++ KestrelManager/test/Kestrel.Client.Tests/ParametersTest.cs
@@ -0,0 +1,23 @@
+﻿using KestrelManager.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Kestrel.Client.Tests
+{
+    public class ParametersTest
+    {
+        [Fact]
+        public void ConstructorTest()
+        {
+            var stringParameters = new[] { "-h","localhost","-a","yourapp","-remotepath","dist","-path","c:\\app" };
+            var parameters = new Parameters(stringParameters);
+            Assert.Equal(parameters.App, "yourapp");
+            Assert.Equal(parameters.Host, "localhost");
+            Assert.Equal(parameters.Path, "c:\\app");
+            Assert.Equal(parameters.RemotePath, "dist");
+        }
+    }
+}
